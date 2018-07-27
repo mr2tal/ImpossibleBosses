708f0389635310a92bc85d1359361bddee7b7103
Quartzinin <levestol@hotmail.com>
2018-07-27 11:14:13 +0200

    Create player and basic movements.

---
+++ Impossible bosses/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+    public const float movementSpeed = 3f;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+
+    // Update is called once per frame
+    void Update () {
+        float hMov = Input.GetAxis("Horizontal");   // w/s horiztal movement
+        float sMov = Input.GetAxis("Vertical");     // a/d sideways movement
+        float dt = Time.deltaTime;                  // scale with physics time
+        Vector3 changeVector = new Vector3(hMov, 0, sMov);
+        changeVector *= dt * movementSpeed;
+        gameObject.transform.Translate(changeVector);      
+    }
+}
